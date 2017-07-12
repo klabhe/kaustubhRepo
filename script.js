95d699566a65e997ca75c501b92acf1cd0c57a22
klabhe <kaustubhlab@gmail.com>
2017-07-12 23:56:37 +0100

    PartyGame

---
+++ script.js
@@ -0,0 +1,177 @@
+var questions = [
+  {question:"Han toh..what will ya bring?"},
+];
+var randomWord;
+
+/**********
+
+  Thanks for checking out this pen.
+  
+  Credits for the design go to XavierCoulombeM
+  https://dribbble.com/shots/2510592-Simple-register-form
+  
+  This Pen uses no libraries except fonts and should 
+  work on all modern browsers
+  
+  The answers are stored in the `questions` array
+  with the key `value`. 
+
+ **********/
+
+
+    var NumberOfWords = 28
+
+    var words = new BuildArray(NumberOfWords)
+
+// Use the following variables to 
+// define your random words:
+        words[1] = "bicycle"
+        words[2] = "avocado"
+        words[3] = "velcro"
+        words[4] = "fan"
+        words[5] = "wine"
+        words[6] = "jenga"
+        words[7] = "football"
+        words[8] = "flute"
+        words[9] = "magic"
+        words[10] = "television"
+        words[11] = "jelly"
+        words[12] = "thorns"
+        words[13] = "rottweiler"
+        words[14] = "technician"
+        words[15] = "cowp"
+        words[16] = "nescafe"
+        words[17] = "jackhammer"
+        words[18] = "trumpet"
+        words[19] = "wall-e"
+        words[20] = "icecream"
+        words[21] = "zebra"
+        words[22] = "witch"
+        words[23] = "poker"    
+        words[24] = "drums"
+        words[25] = "motorcycle"
+        words[26] = "elephant"
+        words[27] = "ice"
+        words[28] = "apple"
+    
+
+function BuildArray(size){
+    this.length = size
+    for (var i = 1; i <= size; i++){
+    this[i] = null}
+    return this
+}
+
+function PickRandomWord() {
+// Generate a random number between 1 and NumberOfWords
+    var rnd = Math.ceil(Math.random() * NumberOfWords)
+
+    return words[rnd]
+}
+
+
+(function(){
+
+  var tTime = 100  // transition transform time from #register in ms
+  var wTime = 200  // transition width time from #register in ms
+  var eTime = 1000 // transition width time from inputLabel in ms
+
+  // init
+  // --------------
+  var position = 0
+
+  putQuestion()
+
+  progressButton.addEventListener('click', validate)
+  inputField.addEventListener('keyup', function(e){
+    transform(0, 0) // ie hack to redraw
+    if(e.keyCode == 13) validate()
+  })
+
+  // functions
+  // --------------
+
+  // load the next question
+  function putQuestion() {
+    randomWord = PickRandomWord();
+    inputLabel.innerHTML = 'The person before you brought ' +  randomWord + ', ' + 'tum kya laye? ' 
+    inputField.value = ''
+    inputField.type = questions[position].type || 'text'  
+    inputField.focus()
+    showCurrent()
+  }
+  
+  // when all the questions have been answered
+  function done() {
+    
+    // remove the box if there is no next question
+    register.className = 'close'
+    
+    // add the h1 at the end with the welcome text
+    var h1 = document.createElement('h1')
+    h1.appendChild(document.createTextNode('You can come to the party'))
+    setTimeout(function() {
+      register.parentElement.appendChild(h1)     
+      setTimeout(function() {h1.style.opacity = 1}, 50)
+    }, eTime)
+    
+  }
+
+  // when submitting the current question
+  function validate() {
+
+    // set the value of the field into the array
+    questions[position].value = inputField.value
+    
+    // check if the pattern matches
+    if (inputField.value.charAt(0) != randomWord.charAt(0)) wrong() 
+    else ok(function() {
+      // set the progress of the background
+      progress.style.width = ++position * 100 / questions.length + 'vw'
+
+      // if there is a new question, hide current and load next
+      if (questions[position]) hideCurrent(putQuestion)
+      else hideCurrent(done)
+             
+    })
+
+  }
+
+  // helper
+  // --------------
+
+  function hideCurrent(callback) {
+    inputContainer.style.opacity = 0
+    inputProgress.style.transition = 'none'
+    inputProgress.style.width = 0
+    setTimeout(callback, wTime)
+  }
+
+  function showCurrent(callback) {
+    inputContainer.style.opacity = 1
+    inputProgress.style.transition = ''
+    inputProgress.style.width = '100%'
+    setTimeout(callback, wTime)
+  }
+
+  function transform(x, y) {
+    register.style.transform = 'translate(' + x + 'px ,  ' + y + 'px)'
+  }
+
+  function ok(callback) {
+    register.className = ''
+    setTimeout(transform, tTime * 0, 0, 10)
+    setTimeout(transform, tTime * 1, 0, 0)
+    setTimeout(callback,  tTime * 2)
+  }
+
+  function wrong(callback) {
+    register.className = 'wrong'
+    for(var i = 0; i < 6; i++) // shaking motion
+      setTimeout(transform, tTime * i, (i%2*2-1)*20, 0)
+    setTimeout(transform, tTime * 6, 0, 0)
+    setTimeout(callback,  tTime * 7)
+    location.reload();  
+  }
+    
+}())
